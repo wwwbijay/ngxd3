3476e602483cfd420c5d5c7e6ab9cb97b9d658f3
wwwbijay <link2bj@gmail.com>
2023-04-24 21:49:02 +0545

    initialize for d3drag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BarComponent } from './bar/bar.component';
 import { PieComponent } from './pie/pie.component';
 import { ScatterComponent } from './scatter/scatter.component';
+import { D3dragComponent } from './d3drag/d3drag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarComponent,
     PieComponent,
-    ScatterComponent
+    ScatterComponent,
+    D3dragComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/d3drag/d3drag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'd3drag',
+  templateUrl: './d3drag.component.html',
+  styleUrls: ['./d3drag.component.css']
+})
+export class D3dragComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
